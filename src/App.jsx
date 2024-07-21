902fa277272ff9278c20a59a179ecc25d14333da
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-07-21 19:49:14 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import SearchBar from "./components/SearchBar/SearchBar";
+
+export default function App() {
+  return <SearchBar></SearchBar>;
+}
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+const SearchBar = () => {
+  return (
+    <header>
+      <form>
+        <input
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
+};
+
+export default SearchBar;
